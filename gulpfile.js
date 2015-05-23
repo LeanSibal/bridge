cba5a657bb4df617d4c999e5f3fb38d4f8d7532d
Lean Sibal <leansibal@gmail.com>
2015-05-23 22:23:23 +0800

    Added socket.io-client for unit testing in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 var gulp = require('gulp'),
-	nodemon = require('gulp-nodemon');
+	nodemon = require('gulp-nodemon'),
+	io = require('socket.io-client');
 
+gulp.task('unit-testing', function(){
+	var socket = io.connect('http://localhost:3000');
+	socket.on('response',function(response){
+		console.log({
+			'Socket Response' : response
+		});
+	});
+	console.log('unit test');
+});
 gulp.task('nodemon', function(){
 	nodemon({
 		script: 'server.js',
 	}).on('restart', function(){
-		console.log('Server restarted');
+		gulp.run('unit-testing');
 	});
 });
 gulp.task('default', [
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var server = require('http').createServer();
 var io = require('socket.io')(server);
-console.log('Server Started');
+
+
 io.on('connection', function(connection){
-	console.log('Connection');
+	console.log({
+		'Connection' : connection.id
+	});
 	connection.on('event', function(data){
 		console.log('Event');
 	});
